00fd2bfaf0dc785660672652101086390c6a7732
HendP <hendpermana12@gmail.com>
2022-10-04 20:34:04 +0700

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { getUserLogged, putAccessToken } from './utils/network-data';
 import Navigation from './components/Navigation';
