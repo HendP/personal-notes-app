d85b3fcc0be122c37c688a383ee936935776cd11
HendP <hendpermana12@gmail.com>
2022-10-06 16:26:31 +0700

    implement react toastify

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
 import App from './App';
 
 import './styles/style.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
+            <ToastContainer />
             <App />
         </BrowserRouter>
     </React.StrictMode>
